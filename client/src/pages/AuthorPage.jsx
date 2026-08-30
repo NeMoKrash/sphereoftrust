feca132f225f9a81703d7bb1492a7837dcd6a169
Твоё Имя <eabenov001@mial.ru>
2026-08-30 14:39:34 +0500

    Ученик 10В, школа Лицей №48 на странице авторов

--- client/src/pages/AuthorPage.jsx
+++ client/src/pages/AuthorPage.jsx
@@ -11,7 +11,7 @@ export default function AuthorPage() {
           <div className="card author-card">
             <div className="author-card__line">
               <span className="author-card__label">Автор:</span>
-              Абенов Эрик, 10В, Лицей №48
+              Абенов Эрик, ученик 10В, школа Лицей №48
             </div>
             <div className="author-card__line">
               <span className="author-card__label">Руководитель:</span>
